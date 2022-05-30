0b8d41f8263b88692dbb4adf5ba7428d4f6c1b7c
Kundan <kundan kamble>
2022-05-30 01:19:33 -0700

    [KUNDAN] Added Frequency of words program

---
+++ HashTable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HashTable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hash Table");
+            HashTable<string, int> hashtable = new HashTable<string, int>(5);
+            string sentence = "To be or not to be";
+            string[] words = sentence.ToLower().Split(' ');
+            foreach (string word in words)
+            {
+                int value = hashtable.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                    hashtable.Add(word, value);
+                }
+                else
+                {
+                    value += 1;
+                    hashtable.Add(word, value);
+                }
+            }
+            int frequencyTo = hashtable.Get("to");
+            int frequencyNot = hashtable.Get("not");
+            Console.WriteLine("Frequency of to " + frequencyTo);
+            Console.WriteLine("Frequency of not " + frequencyNot);
+        }
+    }
+}
+
